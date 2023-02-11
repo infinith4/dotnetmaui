6303c915bdedbaff4f201c89f09adcbbde2522f7
infinith4 <infinith4@gmail.com>
2023-02-11 10:28:40 +0900

    new app

---
+++ mauiapp01/mauiapp01/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace mauiapp01;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ mauiapp01/mauiapp01/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace mauiapp01;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
