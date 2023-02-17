b3f70cb16fe53fd88cebc2364db7f46aa9acef23
infinith4 <infinith4@gmail.com>
2023-02-17 20:31:58 +0900

    add

---
+++ udemy/MVVMDemo/MVVMDemo/MVVM/ViewModels/PersonViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MVVMDemo.MVVM.ViewModels
+{
+	public class PersonViewModel
+	{
+        public Models.Person Person { get; set; }
+		public PersonViewModel()
+		{
+
+            Person = new Models.Person
+            {
+                Name = "Hiroshi",
+                Age = 100
+            };
+        }
+	}
+}
+
---
+++ udemy/MVVMDemo/MVVMDemo/MVVM/Views/PersonView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MVVMDemo.MVVM.ViewModels;
+
+namespace MVVMDemo.MVVM.Views;
+
+public partial class PersonView : ContentPage
+{
+	public PersonView()
+	{
+		InitializeComponent();
+		//var person = new Models.Person
+		//{
+		//	Name = "Hiroshi",
+		//	Age = 32
+		//};
+		BindingContext = new PersonViewModel();
+	}
+}
