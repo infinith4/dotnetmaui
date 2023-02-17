fdbfac33d107c2c4b94ac49ee935501d4ae6b834
infinith4 <infinith4@gmail.com>
2023-02-17 21:03:46 +0900

    add

--- udemy/MVVMDemo/MVVMDemo/MVVM/ViewModels/PersonViewModel.cs
+++ udemy/MVVMDemo/MVVMDemo/MVVM/ViewModels/PersonViewModel.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using MVVMDemo.MVVM.Models;
 namespace MVVMDemo.MVVM.ViewModels
 {
 	public class PersonViewModel
 	{
-        public Models.Person Person { get; set; }
+        public Person Person { get; set; }
 		public PersonViewModel()
 		{
 
             Person = new Models.Person
             {
-                Name = "Hiroshi",
-                Age = 100
+                Name = "infinith4",
+                Age = 10,
+                Married = true,
+                BirthDate = new DateTime(1989, 12, 7),
+                Weight = 60,
+                Lunchtime = new TimeSpan(10,0,0)
             };
         }
 	}
